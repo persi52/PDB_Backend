c42dfd48984495e09b95987ecc171264a4843280
Bartlomiej Zima <bartlomiej.zima@o2.pl>
2021-12-09 22:06:01 +0100

    asdad

--- models/db.js
+++ models/db.js
@@ -1,12 +1,14 @@
 const {Pool} = require('pg');
-require("dotenv").config();
 
 const pool = new Pool({
-    user: ${process.env.PG_USER},
-    password: ${process.env.PG_PASSWORD}, 
-    host: ${process.env.PG_HOST},
-    port: ${process.env.PG_PORT},
-    database: ${process.env.PG_DATABASE}
+    user: "olcpuspklcyuvx",
+    password: "8ca94e9bbe858fd6dd9b904c18c3518eedccb7a460a5f3780492b9a3055b0fbd", 
+    host: "ec2-3-211-228-251.compute-1.amazonaws.com",
+    port: 5432,
+    database: "d9ee4pkbrup9lt",
+	ssl: {
+		rejectUnauthorized: false
+	}
 });
 
 module.exports = pool;
