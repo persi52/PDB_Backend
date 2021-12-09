32e2a3e590fab1095087251c42f917eb5f6c8d56
Bartlomiej Zima <bartlomiej.zima@o2.pl>
2021-12-09 21:49:44 +0100

    database uuuu

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    password: "imaginaris", //Zima-"imaginaris" Zazi-"zaq1@WSX"
-    host: "localhost",
-    port: 5433,
-    database: "imaginaris"
+    user: "olcpuspklcyuvx",
+    password: "8ca94e9bbe858fd6dd9b904c18c3518eedccb7a460a5f3780492b9a3055b0fbd", 
+    host: "ec2-3-211-228-251.compute-1.amazonaws.com",
+    port: 5432,
+    database: "d9ee4pkbrup9lt"
 });
 
 module.exports = pool;
