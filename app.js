bf3bd96669ab9e049e4e651923cbb3ca4423e480
Bartlomiej Zima <bartlomiej.zima@o2.pl>
2021-12-20 17:01:04 +0100

    token

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json()); // zeby apka akceptowala jsony
 app.set("trust proxy", 1);
 app.use(session({
     cookie: { 
-        domain:'.netlify.app',
+        domain:'https://pdbmovies.netlify.app',
         sameSite : 'none',
         secure : true
     },
@@ -23,7 +23,7 @@ app.use(session({
     resave: false, //should we resave session variables if nothing is changed
     saveUninitialized: false //do we wanna save session if there is no value placed in a session
 }));
-app.use(cors({credentials: true, origin:'https://pdbmovies.netlify.app'}));
+app.use(cors({credentials: true, origin:'https://localhost:3000'}));
 app.use(cookieParser());
 app.use(function(req, res, next) {  //cross origin resource sharing, pozwolenie na łączenie sie frontendu i backendu 
    // res.header("Access-Control-Allow-Origin", 'https://pdbmovies.netlify.app');
